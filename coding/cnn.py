956ddc5e4915407d7986c677d526b60adaeddd14
rob <rob@rob>
2017-12-19 14:31:00 +0100

    add new cnn model

--- coding/cnn.py
+++ coding/cnn.py
@@ -21,16 +21,46 @@ import matplotlib.pyplot as plt
 def CNN(input_, num_classes):
     #print input_shape
     model = Sequential()
-    model.add(Conv2D(16, kernel_size=(5, 5),
-                     activation='relu',
-                     input_shape=input_))
-    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
+    model.add(Conv2D(16, (3, 3), border_mode='same', input_shape=input_))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    # model.add(Conv2D(16, (3, 3), border_mode='same'))
+    # model.add(BatchNormalization())
+    # model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
     model.add(Dropout(0.5))
-    model.add(Conv2D(64, (5, 5), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    # model.add(Conv2D(32, (3, 3), border_mode='same'))
+    # model.add(BatchNormalization())
+    # model.add(Activation('relu'))
+    model.add(Conv2D(32, (3, 3), border_mode='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.5))
+
+    model.add(Conv2D(64, (3, 3), border_mode='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Dropout(0.5))
+
+    # model.add(Conv2D(128, (7, 7), border_mode='same'))
+    # model.add(BatchNormalization())
+    # model.add(Activation('relu'))
+    # model.add(MaxPooling2D(pool_size=(2,2)))
+    # model.add(Dropout(0.5))
+
     model.add(Flatten())
-    model.add(Dense(1000, activation='relu'))
-    model.add(Dense(num_classes, activation='softmax'))
+
+    # Fully connected layer
+    model.add(Dense(512))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(10))
+    model.add(Activation('softmax'))
+
     return model
 
 # Generate images according to batch size
